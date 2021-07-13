3d58512dfc95efef4d7db6f923ec5e69577e822e
Jochen Wezel <jwezel@compumaster.de>
2021-07-13 17:22:12 +0200

    added enum VariantType

--- CompuMaster.VisualBasicCompatibility/Information.cs
+++ CompuMaster.VisualBasicCompatibility/Information.cs
@@ -54,5 +54,29 @@ namespace CompuMaster.VisualBasicCompatibility
             True = -1,
             False = 0
         }
+
+        public enum VariantType : int
+        {
+            Array = 8192,
+            Boolean = 11,
+            Byte = 17,
+            Char = 18,
+            Currency = 6,
+            DataObject = 13,
+            Date = 7,
+            Decimal = 14,
+            Double = 5,
+            Empty = 0,
+            Error = 10,
+            Integer = 3,
+            Long = 20,
+            Null = 1,
+            Object = 9,
+            Short = 2,
+            Single = 4,
+            String = 8,
+            UserDefinedType = 36,
+            Variant = 12
+        }
     }
 }

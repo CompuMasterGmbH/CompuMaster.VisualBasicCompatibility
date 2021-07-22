794a52d704255770cdb94a4a226b086a4398cc01
Thomas Schädler <tschaedler@compuamster.de>
2021-07-22 09:11:34 +0200

    add more char-variables for ControlChars-class
    also add testcases

--- CompuMaster.VisualBasicCompatibility/ControlChars.cs
+++ CompuMaster.VisualBasicCompatibility/ControlChars.cs
@@ -8,5 +8,9 @@ namespace CompuMaster.VisualBasicCompatibility
         public const char Lf = '\n';
         public const char Tab = '\t';
         public const string CrLf = "\r\n";
+        public const char VerticalTab = '\v';
+        public const char NullChar = '\0';
+        public const char Back = '\b';
+        public const char FormFeed = '\f';
     }
 }
